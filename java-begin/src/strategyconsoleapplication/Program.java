0ba92f0b2ee78872db8e594e8909371440eb190b
Kazi Abdullah Saikat <kazi.saikat@bouvet.no>
2013-11-19 15:52:27 +0100

    Include pico-container

--- java-begin/src/strategyconsoleapplication/Program.java
+++ java-begin/src/strategyconsoleapplication/Program.java
@@ -4,12 +4,7 @@ public class Program {
 
     public static void main(String [] args) {
 
-        if (args.length != 2) {
-            System.out.println("usage: program gender sales-mode");
-            System.out.printf("gender: { %s }\n", Gender.MALE.name().toLowerCase() + ", " + Gender.FEMALE.name().toLowerCase());
-            System.out.printf("sales-mode: { %s }\n", getAvailableSalesModes());
-            System.exit(- 1);
-        }
+        ProgramUsage.checkUsage(args);
 
         Gender gender = Gender.valueOf(args[0].toUpperCase());
         SalesMode salesMode = SalesMode.valueOf(args[1].toUpperCase());
@@ -38,18 +33,5 @@ public class Program {
             recommendedProduct = new Product("SomeProduct", "Use some default recommendation??");
         }
         System.out.println(recommendedProduct);
-    }
-
-    private static String getAvailableSalesModes() {
-        StringBuilder builder = new StringBuilder();
-        SalesMode [] salesModes = SalesMode.values();
-        for (int i = 0; i < salesModes.length; i++) {
-            SalesMode mode =  salesModes[i];
-            builder.append(mode.name().toLowerCase());
-            if (i < salesModes.length - 1) {
-                builder.append(", ");
-            }
-        }
-        return builder.toString();
     }
 }
--- java-end/src/strategyconsoleapplication/product/ProductRecommender.java
+++ java-end/src/strategyconsoleapplication/product/ProductRecommender.java
@@ -2,7 +2,10 @@ package strategyconsoleapplication.product;
 
 import strategyconsoleapplication.Customer;
 import strategyconsoleapplication.Product;
+import strategyconsoleapplication.SalesMode;
 
 public interface ProductRecommender {
+    boolean supports(SalesMode salesMode);
+
     Product recommend(Customer customer);
 }
